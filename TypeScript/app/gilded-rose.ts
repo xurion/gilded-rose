2880fb8c26e783efbf252063a41f395caa69e319
Dean James <xurion@hotmail.com>
2020-07-19 21:19:40 +0100

    Swapped for loop in favour of forEach.

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,86 +19,78 @@ export class GildedRose {
 
     updateQuality() {
         //loop over every item to asses/update quality
-        for (let i = 0; i < this.items.length; i++) {
+        this.items.forEach((item) => {
             //everything except affinage cheese and backstage passes
             if (
-                this.items[i].name != 'Aged Brie' &&
-                this.items[i].name !=
-                    'Backstage passes to a TAFKAL80ETC concert'
+                item.name != 'Aged Brie' &&
+                item.name != 'Backstage passes to a TAFKAL80ETC concert'
             ) {
                 //if the item is still worth something
-                if (this.items[i].quality > 0) {
+                if (item.quality > 0) {
                     //ignore the beautiful legendary items
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                    if (item.name != 'Sulfuras, Hand of Ragnaros') {
                         // reduce the quality by 1
-                        this.items[i].quality = this.items[i].quality - 1;
+                        item.quality = item.quality - 1;
                     }
                 }
                 // Affinage cheese or a backstage pass
             } else {
                 //if the quality is less than the standard item maximum (ignores 50+ quality legendary items?)
-                if (this.items[i].quality < 50) {
+                if (item.quality < 50) {
                     //increase quality by 1
-                    this.items[i].quality = this.items[i].quality + 1;
+                    item.quality = item.quality + 1;
                     if (
-                        this.items[i].name ==
-                        'Backstage passes to a TAFKAL80ETC concert'
+                        item.name == 'Backstage passes to a TAFKAL80ETC concert'
                     ) {
                         //if the no of days left to sell is 10 or lower
-                        if (this.items[i].sellIn < 11) {
+                        if (item.sellIn < 11) {
                             //increase quality by 1
-                            this.items[i].quality = this.items[i].quality + 1;
+                            item.quality = item.quality + 1;
                         }
                         //there's only 5 or less days left to sell
-                        if (this.items[i].sellIn < 6) {
+                        if (item.sellIn < 6) {
                             //increase quality by 1
-                            this.items[i].quality = this.items[i].quality + 1;
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
             }
 
             //if not legendary
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            if (item.name != 'Sulfuras, Hand of Ragnaros') {
                 //reduce sellIn by 1
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+                item.sellIn = item.sellIn - 1;
             }
 
             //days left to sell is passed
-            if (this.items[i].sellIn < 0) {
+            if (item.sellIn < 0) {
                 //not affinage cheese
-                if (this.items[i].name != 'Aged Brie') {
+                if (item.name != 'Aged Brie') {
                     //also not backstage pass
                     if (
-                        this.items[i].name !=
-                        'Backstage passes to a TAFKAL80ETC concert'
+                        item.name != 'Backstage passes to a TAFKAL80ETC concert'
                     ) {
                         //these ifs tho
-                        if (this.items[i].quality > 0) {
+                        if (item.quality > 0) {
                             //srsly
-                            if (
-                                this.items[i].name !=
-                                'Sulfuras, Hand of Ragnaros'
-                            ) {
+                            if (item.name != 'Sulfuras, Hand of Ragnaros') {
                                 //nice, a conclusion
-                                this.items[i].quality =
-                                    this.items[i].quality - 1;
+                                item.quality = item.quality - 1;
                             }
                         }
                         //backstage pass that has expired
                     } else {
                         //sets quality to 0, but why not explicitly set it to just 0?
-                        this.items[i].quality =
-                            this.items[i].quality - this.items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                     //affinage cheese!
                 } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
-        }
+        });
 
         return this.items;
     }
